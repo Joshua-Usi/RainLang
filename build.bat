d17e412578dbf8dfa1ff861392c02b2259829575
Joshua Usi <ExperimentatorsY@gmail.com>
2025-11-09 17:27:38 +1100

    Cleanup repo

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
-call clean.bat
+del /s /q out/*.class >nul 2>&1
+echo Cleaned up class files
 rem echo Generating AST file
 rem src\ast_builder.py
 echo Adding standard library
--- clean.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-del /s /q out/*.class >nul 2>&1
-echo Cleaned up class files
